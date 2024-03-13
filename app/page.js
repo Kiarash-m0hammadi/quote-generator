d08c9e0935462676bb4c1a056b6ec0df1be331e2
Kiarash-m0hammadi <kiarash7.mohammadi@gmail.com>
2024-03-13 21:09:59 +0330

    Update categorySelector and page styles

--- app/page.js
+++ app/page.js
@@ -15,8 +15,8 @@ export default function QuoteApp() {
   };
 
   return (
-    <div className="justify-center overflow-hidden text-center h-screen w-screen mx-auto rounded-md relative flex flex-col items-center antialiased">
-      <h1 className="relative z-10 text-4xl md:text-7xl pb-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 my-7 text-center font-sans font-bold">
+    <div className="md:justify-center overflow-hidden text-center h-screen w-screen mx-auto rounded-md relative flex flex-col items-center antialiased">
+      <h1 className="relative z-10 mt-16 text-4xl md:text-7xl pb-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 my-7 text-center font-sans font-bold">
         Quote App
       </h1>
       <CategorySelector
@@ -28,7 +28,7 @@ export default function QuoteApp() {
       {quotes.length > 0 && <Quote quotes={quotes} />}
       <button
         onClick={fetchNewQuotes}
-        className="inline-flex h-12 my-12 animate-shimmer items-center justify-center rounded border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors hover:bg-slate-400 hover:text-white"
+        className="inline-flex h-12 my-10 md:my-12  animate-shimmer items-center justify-center rounded border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors hover:bg-slate-400 hover:text-white"
       >
         New Quotes
       </button>
@@ -37,7 +37,7 @@ export default function QuoteApp() {
       </div>
       <a
         href="https://github.com/Kiarash-m0hammadi"
-        className="fixed bottom-0 right-0 px-3 py-2 border border-white text-white rounded-tl-full rounded-br-full font-bold transform hover:-translate-y-1 transition duration-400 shadow-[0_0_0_3px_#000000_inset] hover:bg-gray-500 hover:text-slate-200"
+        className="fixed bottom-1 right-1 px-3 py-2 translate-x-5 translate-y-1 md:translate-x-0 md:translate-y-0 scale-75 md:scale-100 border border-white text-white rounded-tl-full rounded-br-full font-bold transform hover:-translate-y-1 transition duration-400 shadow-[0_0_0_3px_#000000_inset] hover:bg-gray-500 hover:text-slate-200"
       >
         Creator&apos;s github
       </a>
